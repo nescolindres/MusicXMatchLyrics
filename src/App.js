4d5a050f58dd164e8998d876e4c514e3d34bcd1e
Nescolindres <nestorcolindres17@gmail.com>
2022-12-21 15:01:26 -0800

    Added Index.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
-import './App.css';
+import "./App.css";
+import Navbar from "./components/layout/Navbar";
+import Index from "./components/layout/Index";
+
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import React from "react";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello</h1>  
-    </div>
+    <Router>
+      <React.Fragment>
+        <Navbar />
+        <div className="container">
+          <Routes>
+            <Route exact path="/" element ={<Index/>} />
+          </Routes>
+        </div>
+      </React.Fragment>
+    </Router>
   );
 }
 
